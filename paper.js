1a3268edaea324736ad4d6cfec9d3bc9d7674fa5
RehanShah07 <74101385+RehanShah07@users.noreply.github.com>
2020-12-30 16:52:28 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class paper {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+      }
+      this.body = Bodies.circle(100, 200, 3,options);
+      this.radius=3;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x=mouseX;
+      pos.y=mouseY;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke('cyan');
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
